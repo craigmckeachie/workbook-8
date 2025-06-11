9671602538edcc9bde300ffb6a620d2331026e0f
Craig McKeachie <craigdmckeachie@gmail.com>
2025-06-11 08:58:43 -0400

    sakila prepared statement with placeholder

--- sakila/src/main/java/com/pluralsight/Main.java
+++ sakila/src/main/java/com/pluralsight/Main.java
@@ -1,38 +1,55 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         String username = args[0];
         String password = args[1];
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Search for films that start with: ");
+        String searchTerm = scanner.nextLine() + "%";
+
         String sql = """
                  select * 
-                 from film;
+                 from film
+                 where title like ?;
                 """;
 
         try (
                 //create a connection
                 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", username, password);
                 //create a SQL statement
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                //execute the statement/query
-                ResultSet resultSet = preparedStatement.executeQuery()) {
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
 
-            //print header row
-            System.out.printf("%-4s %-40s %10s%n", "Id", "Title", "Release Year");
-            System.out.println("_________________________________________________________________________________");
+        ) {
 
-            //loop through the results and display them
-            while (resultSet.next()) {
-                int id = resultSet.getInt("film_id");
-                String title = resultSet.getString("title");
-                int releaseYear = resultSet.getInt("release_year");
+            //execute the statement/query
+            preparedStatement.setString(1, searchTerm);
 
-                //print row
-                System.out.printf("%-4d %-40s %10d%n", id, title, releaseYear);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                //print header row
+                System.out.printf("%-4s %-40s %10s%n", "Id", "Title", "Release Year");
+                System.out.println("_________________________________________________________________________________");
+
+                //loop through the results and display them
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("film_id");
+                    String title = resultSet.getString("title");
+                    int releaseYear = resultSet.getInt("release_year");
+
+                    //print row
+                    System.out.printf("%-4d %-40s %10d%n", id, title, releaseYear);
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
+
+
         } catch (SQLException e) {
             //display user friendly error message
             System.out.println("There was an error retrieving the films. Please try again or contact support.");
